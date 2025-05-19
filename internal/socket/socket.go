0871eaec3a3ab2b64d27f8beec9a2c5d6e5e313b
stanleydv12 <sdaveteherag@gmail.com>
2025-05-19 22:33:05 +0700

    refactor(socket): update socket interface

---
+++ internal/socket/socket.go
@@ -0,0 +1,16 @@
+//go:build linux
+package socket
+
+type SocketOptions struct {
+	NonBlocking bool
+	ReuseAddr   bool
+	Type        int
+}
+
+type SocketManager interface {
+	CreateSocket(options *SocketOptions) (fd int, err error)
+	CloseSocket(fd int) error
+	BindSocket(fd int, address string, port int) error
+	StartListening(fd int) error
+	AcceptConnection(fd int) (int, error)
+}
