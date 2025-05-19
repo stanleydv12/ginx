a194f55afa389619432638a519e8426c4358939c
stanleydv12 <sdaveteherag@gmail.com>
2025-05-19 22:33:53 +0700

    feat: implement TCP server with connection handling

--- cmd/main.go
+++ cmd/main.go
@@ -1,43 +1,99 @@
+//go:build linux
+
 package main
 
 import (
 	"os"
 
-	"github.com/joho/godotenv"
 	"ginx/internal/config"
+	"ginx/internal/socket"
+	"ginx/internal/socket/linux"
 	"ginx/pkg/logger"
 )
+
+type Server struct {
+	listenFd    int
+	config      *config.ServerConfig
+	socket      socket.SocketManager
+}
+
+func NewServer(config *config.ServerConfig, socket socket.SocketManager) *Server {
+	return &Server{
+		config: config,
+		socket: socket,
+	}
+}
+
+func (s *Server) Start() error {
+	// Test open and close socket
+	fd, err := s.socket.CreateSocket(nil)
+	if err != nil {
+		logger.Error("Failed to create socket", "error", err)
+		return err
+	}
+
+	if err := s.socket.BindSocket(fd, s.config.Server.Address, s.config.Server.Port); err != nil {
+		logger.Error("Failed to bind socket", "error", err)
+		return err
+	}
+
+	if err := s.socket.StartListening(fd); err != nil {
+		logger.Error("Failed to listen on socket", "error", err)
+		return err
+	}
+	s.listenFd = fd
+
+	for {
+		connFd, err := s.socket.AcceptConnection(s.listenFd)
+		if err != nil {
+			continue
+		}
+
+		if connFd == -1 {
+			continue
+		}
+
+		logger.Info("Accepted connection", "fd", connFd)
+
+		go func(connFd int) {
+			defer s.socket.CloseSocket(connFd)
+
+			// TODO: Handle connection
+		}(connFd)
+	}
+}
 
 func main() {
 	// Initialize logger
 	logger.Default()
-
 	logger.Info("Starting ginx proxy server")
 
-	// Load .env file if it exists
-	if err := godotenv.Load(); err != nil {
-		logger.Info("No .env file found or error loading .env file", "error", err)
-	}
-
-	// Get config path from environment variable, default to "config/development.yaml"
-	configPath := os.Getenv("CONFIG_PATH")
-	if configPath == "" {
-		configPath = "config/development.yaml"
-	}
-
 	// Load configuration
-	cfg, err := config.LoadConfig(configPath)
+	cfg, err := config.LoadConfig()
 	if err != nil {
-		logger.Error("Failed to load config", "path", configPath, "error", err)
+		logger.Error("Failed to load config", "error", err)
 		os.Exit(1)
 	}
 
-	logger.Info("Config loaded successfully", 
+	logger.Info("Config loaded successfully",
 		"port", cfg.Server.Port,
 		"async_method", cfg.Server.AsyncMethod,
 		"load_balancer", cfg.Server.LoadBalancer,
 		"upstream_servers", cfg.Server.UpstreamServers,
 	)
 
-	// TODO: Initialize and start the server
+	// Initialize socket manager
+	socketManager := linux.NewLinuxSocketManager()
+
+	// Initialize server
+	server := NewServer(cfg, socketManager)
+
+	// Start server
+	if err := server.Start(); err != nil {
+		logger.Error("Failed to start server", "error", err)
+		os.Exit(1)
+	}
+	defer server.socket.CloseSocket(server.listenFd)
+
+	logger.Info("Server started successfully")
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.23.2
 
 require (
 	github.com/joho/godotenv v1.5.1
+	golang.org/x/sys v0.33.0
 	gopkg.in/yaml.v2 v2.4.0
 )
